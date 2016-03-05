87a91405b5ecbd0befed3538bffe124e53b7cb37
Anthony Granger <grangeranthony@gmail.com>
2016-03-05 22:33:57 +0100

    Ajoute le lancement du serveur symfony à gulp

--- gulpfile.js
+++ gulpfile.js
@@ -4,12 +4,21 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var sourcemaps = require('gulp-sourcemaps');
 var browserSync = require('browser-sync').create();
+var exec = require('child_process').exec;
 
-gulp.task('default', ['browserSync', 'sass', 'sass:watch'], function() {
+gulp.task('default', ['sass'], function() {
 
 });
 
-gulp.task('browserSync', function() {
+//Lance le serveur sous 127.0.0.1:3000
+gulp.task('serve', function() {
+	exec('php bin/console server:start', function(err, stdout, stderr) {
+		console.log(stdout);
+	});
+});
+
+//Lance le rafraichissement automatique du navigateur
+gulp.task('browserSync', ['serve'], function() {
   browserSync.init([
   	'app/Resources/assets'
   ], {
@@ -17,7 +26,8 @@ gulp.task('browserSync', function() {
   });
 })
  
-gulp.task('sass', function() {
+//Compile les fichiers sass en un fichier css
+gulp.task('sass', ['browserSync'], function() {
   return gulp.src('./app/Resources/assets/sass/application.scss')
   	.pipe(sourcemaps.init())
     .pipe(sass().on('error', sass.logError))
@@ -27,7 +37,8 @@ gulp.task('sass', function() {
       stream: true
     }));
 });
- 
+
+//Relance automatiquement la compilation si un fichier sass est modifié
 gulp.task('sass:watch', function() {
   gulp.watch('./app/Resources/assets/sass/*.scss', ['sass']);
 });
