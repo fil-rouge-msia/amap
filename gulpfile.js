ea753b66b41b7f8af60764be001585bd65a88cb9
Anthony Granger <grangeranthony@gmail.com>
2016-03-05 17:43:55 +0100

    Ajoute browsersync

--- gulpfile.js
+++ gulpfile.js
@@ -3,17 +3,29 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var sourcemaps = require('gulp-sourcemaps');
+var browserSync = require('browser-sync').create();
 
-gulp.task('default', ['sass', 'sass:watch'], function() {
+gulp.task('default', ['browserSync', 'sass', 'sass:watch'], function() {
 
 });
+
+gulp.task('browserSync', function() {
+  browserSync.init([
+  	'app/Resources/assets'
+  ], {
+  	proxy: '127.0.0.1:8000'
+  });
+})
  
 gulp.task('sass', function() {
   return gulp.src('./app/Resources/assets/sass/application.scss')
   	.pipe(sourcemaps.init())
     .pipe(sass().on('error', sass.logError))
     .pipe(sourcemaps.write())
-    .pipe(gulp.dest('./web/styles'));
+    .pipe(gulp.dest('./web/styles'))
+    .pipe(browserSync.reload({
+      stream: true
+    }));
 });
  
 gulp.task('sass:watch', function() {
