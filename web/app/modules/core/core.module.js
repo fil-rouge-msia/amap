e2da9df724f3b396bab76f000edbadaf89d325a2
Anthony Granger <grangeranthony@gmail.com>
2016-03-08 16:55:44 +0100

    Début des routes frontend

--- web/app/modules/core/core.module.js
+++ web/app/modules/core/core.module.js
@@ -1,3 +1,20 @@
 'use strict';
 
-angular.module('core', []);
+var core = angular.module('core', []);
+
+core.config(['$routeProvider', function($routeProvider) {
+	$routeProvider.when('/',
+	{
+    	templateUrl: 'app/modules/core/partials/home.html',
+    	controller: 'HomeController'
+	})
+	.when('/404',
+	{
+    	templateUrl: 'app/modules/core/partials/404.html'
+	})
+	.otherwise('/404');
+}]);
+
+core.controller('HomeController', ['$scope', function($scope) {
+
+}]);
