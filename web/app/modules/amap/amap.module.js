fd96cb75ea7a592d59d52c0e6f84cc0f9858ede4
Khaeos <kaworu-kisaragi@live.fr>
2016-04-06 09:58:15 +0200

    Connexion
    Ajout form connexion.

--- web/app/modules/amap/amap.module.js
+++ web/app/modules/amap/amap.module.js
@@ -3,9 +3,19 @@
 var amap = angular.module('amap', []);
 
 amap.controller('ListController', ['$scope', 'Restangular', function($scope, Restangular) {
+	console.log('caca');
 	var baseAmaps = Restangular.all('amaps');
 
 	baseAmaps.getList().then(function(amaps) {
 		$scope.amaps = amaps;
 	});
+
+	$scope.delete = function(amap) {
+		amap.remove().then(function(){
+			var idAmap = $scope.amaps.indexOf(amap);
+			$scope.amaps.splice(idAmap,1);
+		});
+	};
+
+
 }]);
