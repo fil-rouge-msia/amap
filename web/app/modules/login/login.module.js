6379ac8b22c89debffb125107be260663bdf25ba
Khaeos <kaworu-kisaragi@live.fr>
2016-04-06 11:42:15 +0200

    Correction indentation login.module.js

--- web/app/modules/login/login.module.js
+++ web/app/modules/login/login.module.js
@@ -2,19 +2,19 @@
 
 var login = angular.module('login', []);
 
- login.config(['$stateProvider', function($stateProvider) {
-$stateProvider.state('login',
- {
- url: '/login',
- templateUrl: 'app/modules/login/partials/login.html'
- })
- .state('login.list',
- {
- url: '/login',
- templateUrl: 'app/modules/login/partials/login.html',
- controller: 'LoginController'
- });
- }]);
+login.config(['$stateProvider', function ($stateProvider) {
+    $stateProvider.state('login',
+        {
+            url: '/login',
+            templateUrl: 'app/modules/login/partials/login.html'
+        })
+    .state('login.list',
+        {
+            url: '/login',
+            templateUrl: 'app/modules/login/partials/login.html',
+            controller: 'LoginController'
+        });
+}]);
 
- login.controller('LoginController', ['$scope', 'Restangular', function($scope, Restangular) {
- }]);
+login.controller('LoginController', ['$scope', 'Restangular', function ($scope, Restangular) {
+}]);
