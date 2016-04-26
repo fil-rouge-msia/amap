380eb6e459d74e45e98b0acc9f842a0372163431
Anthony Granger <grangeranthony@gmail.com>
2016-04-26 14:19:02 +0200

    Corrige les méthodes PUT et POST et généralise la définition de type

--- web/app/app.js
+++ web/app/app.js
@@ -12,5 +12,18 @@
 
 	app.config(['RestangularProvider', function(RestangularProvider) {
 		RestangularProvider.setBaseUrl('/api');
+
+		RestangularProvider.setRequestInterceptor(function(elem, operation, what) {
+			var retElem = elem;
+			if (operation === 'post' || operation === 'put') {
+				if (operation === 'put') {
+					delete retElem.id; //retire l'identifiant de l'objet, déjà présent sur l'url
+				}
+		    	var wrapper = {};
+		    	wrapper[what.substring(0, what.length -1)] = elem; 
+		    	retElem = wrapper;
+			}
+			return retElem;
+		});
 	}]);
 })();
