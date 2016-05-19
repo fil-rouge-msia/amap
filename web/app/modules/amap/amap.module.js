ae021cb1f3067af4e2f70040721b34607f24bd0c
evilchaussette <juliencremon@gmail.com>
2016-05-19 16:25:07 +0200

    Ajout modif (#6)
    * add/modif
    
    * add/edit
    
    * Fusion
    
    * add
    
    * git
    
    * maj
    
    * maj
    
    * correction add/edit

--- web/app/modules/amap/amap.module.js
+++ web/app/modules/amap/amap.module.js
@@ -8,14 +8,35 @@ amap.controller('ListController', ['$scope', 'Restangular', function($scope, Res
 	baseAmaps.getList().then(function(amaps) {
 		$scope.amaps = amaps;
 	});
-
-	$scope.delete = function(amap) {
+        
+        $scope.delete = function(amap) {
 		amap.remove().then(function(){
 			var idAmap = $scope.amaps.indexOf(amap);
 			$scope.amaps.splice(idAmap,1);
 		});
 	};
+}]);
 
+amap.controller('EditAmapController', ['$scope', 'Restangular', '$stateParams', '$state', function($scope, Restangular, $stateParams, $state) {
+        Restangular.one('amaps', $stateParams.id).get().then(function(amap){
+            $scope.amap = amap;
+        });
+        
+        $scope.envoiAmap= function(){
+            $scope.amap.put().then(function(){
+                $state.go('amaps.list');
+            });
+        };
+}]);
 
-
+amap.controller('AddAmapController', ['$scope', 'Restangular', '$state', function($scope, Restangular, $state) {
+        var baseAmap = Restangular.all('amaps');
+        $scope.envoiAmap = {};
+        
+        $scope.envoiAmap= function(){
+            baseAmap.post($scope.amap).then(function() {
+                $state.go('amaps.list');
+            });
+                 
+        };
 }]);
