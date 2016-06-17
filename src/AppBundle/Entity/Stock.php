e87a21f1a111c6a1ede81472cb87d36f7dcc0d0e
Khaeos <kaworu-kisaragi@live.fr>
2016-06-17 14:19:34 +0200

    Backend Stocks Liens
    Producteur -> Stocks
    Produit -> Stocks

--- src/AppBundle/Entity/Stock.php
+++ src/AppBundle/Entity/Stock.php
@@ -35,6 +35,15 @@ class Stock
      */
     private $emballage;
 
+    /**
+     * @ORM\ManyToOne(targetEntity="Produit", inversedBy="stocks")
+     */
+    private $produit;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="Producteur", inversedBy="stocks")
+     */
+    private $producteur;
 
     /**
      * Get id
@@ -92,6 +101,54 @@ class Stock
     public function getEmballage()
     {
         return $this->emballage;
+    }
+
+    /**
+     * Set produit
+     *
+     * @param \AppBundle\Entity\Produit $produit
+     *
+     * @return Contrat
+     */
+    public function setProduit(\AppBundle\Entity\Produit $produit = null)
+    {
+        $this->produit = $produit;
+
+        return $this;
+    }
+
+    /**
+     * Get produit
+     *
+     * @return \AppBundle\Entity\Produit
+     */
+    public function getProduit()
+    {
+        return $this->produit;
+    }
+
+    /**
+     * Set producteur
+     *
+     * @param \AppBundle\Entity\Producteur $producteur
+     *
+     * @return Contrat
+     */
+    public function setProducteur(\AppBundle\Entity\Producteur $producteur = null)
+    {
+        $this->producteur = $producteur;
+
+        return $this;
+    }
+
+    /**
+     * Get producteur
+     *
+     * @return \AppBundle\Entity\Producteur
+     */
+    public function getProducteur()
+    {
+        return $this->producteur;
     }
 }
 
